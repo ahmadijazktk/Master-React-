48ac29e7b542471637d56e2284bbf6bba82298f6
ahmadijazktk <ahmadijazktk1@gmail.com>
2025-08-12 16:03:25 +0500

    Day 2 : Learn Componet in React JS

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css';
+import Component  from './Component/Component.jsx';
+import LearnJsx from './JSX/LearnJsx';
+
+function App(){
+  return <>
+<Component/>
+<Component/>
+  </>
+}
+
+export default App;
