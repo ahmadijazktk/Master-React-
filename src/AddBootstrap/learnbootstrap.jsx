affbdaa7850fa5113254c24319663262254d3430
ahmadijazktk <ahmadijazktk1@gmail.com>
2025-08-14 12:33:40 +0500

    Updates : How to implement Bootstrap in react

--- src/AddBootstrap/learnbootstrap.jsx
+++ src/AddBootstrap/learnbootstrap.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const learnbootstrap = () => {
   return (
     <div>
-      
+      <h1 className='text-danger'>This is how to add class from bootstrap to change color</h1>
     </div>
   )
 }
